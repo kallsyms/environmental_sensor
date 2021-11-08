31c30c45e9a448706228baffafa153cb32747e66
Nick Gregory <computerfreak97@gmail.com>
2021-11-07 22:31:46 -0500

    webserver for remote control

--- environmental_sensor.ino
+++ environmental_sensor.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <WebServer.h>
 #include <InfluxDbClient.h>
 #include <InfluxDbCloud.h>
 #include <Adafruit_SCD30.h>
@@ -11,6 +12,10 @@ Adafruit_VEML7700 veml = Adafruit_VEML7700();
 InfluxDBClient client(influxdb_url, influxdb_org, influxdb_bucket, influxdb_token, InfluxDbCloud2CACert);
 
 Point sensor("environment");
+
+WebServer server(80);
+
+unsigned scd30_temp_offset = 250; // seems to be high by ~2.5degC
 
 void setup() {
   while (!Serial) { delay(10); }
@@ -34,6 +39,8 @@ void setup() {
     Serial.println("SCD30 not found. Halting.");
     while (1) { delay(10); }
   }
+  scd30.setMeasurementInterval(4);
+  scd30.setTemperatureOffset(scd30_temp_offset);
   Serial.println("SCD30 initialized");
 
   // VEML7700 (lux)
@@ -43,31 +50,55 @@ void setup() {
   }
 
   veml.setGain(VEML7700_GAIN_1_4);
-  veml.setIntegrationTime(VEML7700_IT_50MS);
+  veml.setIntegrationTime(VEML7700_IT_100MS);
 
   Serial.println("VEML7700 initialized");
+
+  server.on("/scd30_recal", handle_scd30_recal);
+  server.on("/scd30_temp_offset", handle_scd30_temp_offset);
+  server.onNotFound(handle_404);
+  
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
-void loop() {
-  sensor.clearFields();
+unsigned tick = 0;
 
-  if (scd30.dataReady()){
-    if (scd30.read()) {
-      sensor.addField("temperature", scd30.temperature);
-      sensor.addField("rh", scd30.relative_humidity);
-      sensor.addField("co2", scd30.CO2);
-    } else {
-      Serial.println("Error reading from SCD30");
+void loop() {
+  tick++;
+  
+  server.handleClient();
+
+  if (tick % 5 == 0) {
+    sensor.clearFields();
+  
+    if (scd30.dataReady()){
+      if (scd30.read()) {
+        sensor.addField("temperature", scd30.temperature);
+        sensor.addField("rh", scd30.relative_humidity);
+        sensor.addField("co2", scd30.CO2);
+      } else {
+        Serial.println("Error reading from SCD30");
+      }
+    }
+  
+    sensor.addField("lux", veml.readLux());
+  
+    // TODO: check and reconnect wifi if needed
+    if (!client.writePoint(sensor)) {
+      Serial.print("InfluxDB write failed: ");
+      Serial.println(client.getLastErrorMessage());
     }
   }
 
-  sensor.addField("lux", veml.readLux());
+  delay(1000);
+}
 
-  // TODO: check and reconnect wifi if needed
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
+void handle_scd30_recal() {
+  scd30.forceRecalibrationWithReference(400);
+  server.send(200, "text/plain", "ok");
+}
 
-  delay(5000);
+void handle_404() {
+  server.send(404, "text/plain", "Not found");
 }
